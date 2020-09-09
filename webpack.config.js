31b454c3bf69c7cbb435247d8b73c356d36e1e4d
Amol Gholap <amghola@microsoft.com>
2020-09-09 10:17:40 -0700

    Changed till module 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  entry: ['@babel/polyfill', './lib/components/Index.js'],
+  entry: ['@babel/polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public/'),
     filename: 'bundle.js',
